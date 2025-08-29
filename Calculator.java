0819b42229d138bf5e270d03214d82961fb07372
Mounika802 <mounika837472@gmail.com>
2025-08-29 12:37:52 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,9 @@
-Public Class Calculator{
-Public int add(int a,int b){return a+b;}
-Public int substract(int a,int b){return a-b;}
-Public int multiply(int a,int b){return a*b;}
-Public int divide(int a,int b){if (b==0) throw new ArthmeticException("Cannot divide zero");by return a\b;}
-Public Static void main(String[] args){
+public Class Calculator{
+public int add(int a,int b){return a+b;}
+public int substract(int a,int b){return a-b;}
+public int multiply(int a,int b){return a*b;}
+public int divide(int a,int b){if (b==0) throw new ArthmeticException("Cannot divide zero");by return a\b;}
+public Static void main(String[] args){
 	Calculator calc=new Calculator();
 	System.out.println("Adiition:"+calc.add(10,5));
 	System.out.println("Substraction:"+calc.substract(10,5));
