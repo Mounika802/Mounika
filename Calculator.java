f00b49add8bdb6077e925d4a453e748dba201b1a
Mounika802 <mounika837472@gmail.com>
2025-08-29 12:40:15 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,30 @@
-public Class Calculator{
-public int add(int a,int b){return a+b;}
-public int substract(int a,int b){return a-b;}
-public int multiply(int a,int b){return a*b;}
-public int divide(int a,int b){if (b==0) throw new ArthmeticException("Cannot divide zero");by return a\b;}
-public Static void main(String[] args){
-	Calculator calc=new Calculator();
-	System.out.println("Adiition:"+calc.add(10,5));
-	System.out.println("Substraction:"+calc.substract(10,5));
-	System.out.println("Multiplication:"+calc.multiply(10,5));
-	System.out.println("Division:"+calc.divide(10,5));
-}
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
 }
